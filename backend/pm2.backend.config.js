01e41605edf7e12488ecc969282b7d3687841747
Abdiel Reyes <abykings1@hotmail.com>
2023-11-25 17:58:19 -0600

    added pm2 config

--- backend/pm2.backend.config.js
+++ backend/pm2.backend.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: "hidrometer_backend", // Name of your application
-  script: "./backend/index.js", // Entry point of your application
+  script: "index.js", // Entry point of your application
   interpreter: "/home/ubuntu/.bun/bin/bun", // Path to the Bun interpreter
 };
--- frontend/pm2.frontend.config.js
+++ frontend/pm2.frontend.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: "hidrometer_frontend", // Name of your application
-  script: "run dev ./frontend/index.js", // Entry point of your application
+  script: "bun", // Entry point of your application
+  args: "run dev", // Arguments to pass with the script, when running
   interpreter: "/home/ubuntu/.bun/bin/bun", // Path to the Bun interpreter
 };
