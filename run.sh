9d9d33266ed6e318325457f13a367fb79b3de159
Abdiel Reyes <abykings1@hotmail.com>
2023-12-17 17:21:07 -0600

    added build script

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-cd frontend
-pm2 start "bun run dev" --name "frontend"
+
 cd ../backend
 pm2 start "bun run start" --name "backend"
--- stop.sh
+++ stop.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-pm2 stop frontend
 pm2 stop backend
